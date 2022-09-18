fecf4e9f5ad0bf26c59b408a5b8050b1d6b0aa23
manbalboy <manbalboy@hanmail.net>
2022-09-19 00:04:14 +0900

    doit typescript 72p 까지 학습

--- ch02/src/index.ts
+++ ch02/src/index.ts
@@ -7,7 +7,7 @@ const chance = new Chance();
 
 let persons: IPerson[] = R.range(0, 2).map((n:number) => new Person(chance.name(), chance.age()));
 
-console.log('persons', persons)
+console.log('persons', persons);
 
 const testMakePerson = (): void => {
   let jane: IPerson = makePerson('Jane', 24);
